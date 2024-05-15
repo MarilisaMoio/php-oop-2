fad541696d13e60b46e4f595f3f0d195c437a25b
Marilisa Moio <moiomarilisa@gmail.com>
2024-05-15 15:43:52 +0200

    spooky scary skeleton

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Animal Shop</title>
+</head>
+<body>
+    <h1>Layout croccante per appassionati di croccantini</h1>
+</body>
+</html>
